65b8e3708988cd646c6d36cb21ac462ce007adf0
Andreii Yefimov <andrewwilliamsond@gmail.com>
2020-06-08 13:09:22 +0300

    1st step: shortest path drawing

--- Assets/Scripts/ShortestOpenPath.cs
+++ Assets/Scripts/ShortestOpenPath.cs
@@ -43,7 +43,7 @@ namespace ShortestOpenPath_Algorithm
         private Transform connectionRoot;
 
         [SerializeField]
-        private List<GameObject> forest_;
+        private List<GameObject> drawForest;
         [SerializeField]
         private List<Tree> trees;
         [SerializeField]
@@ -56,15 +56,9 @@ namespace ShortestOpenPath_Algorithm
         [SerializeField]
         private List<Edge> edges;
 
-        private bool initStep;
-        private List<int> visitedIds;
-        List<int> colors;
-
         private void Start()
         {
-            forest_ = new List<GameObject>();
-            visitedIds = new List<int>();
-            colors = new List<int>();
+            drawForest = new List<GameObject>();
         }
 
         public void CalculationScenario()
@@ -75,27 +69,24 @@ namespace ShortestOpenPath_Algorithm
             SpawnTrees(trees);
 
             // setting connection with nearest neighbour
-            /* for (int i = 0; i < forest_.Count; i++)
-                 SetNearestNeighbour(trees[i], trees);*/
+            for (int i = 0; i < drawForest.Count; i++)
+                 SetNearestNeighbour(trees[i], trees);
 
-            SetNearestNeighbour(trees[0], trees);
+            DrawEdges();
+        }
 
-            /*for (int i = 0; i < forest.Count; i++)
-                SetNearestNeighbour(trees[i], trees, i);*/
-
-            // draw set connections
-            /*for (int i = 0; i < edges.Count; i++)
+        private void DrawEdges() {
+            for (int i = 0; i < edges.Count; i++)
             {
-                GameObject connection = Instantiate(connectionPrefab, connectionRoot);
-                connection.GetComponent<LineRenderer>().positionCount = 2;
-
                 if (edges[i].points[0].treeID == edges[i].points[1].treeID)
                     continue;
 
-                connection.GetComponent<LineRenderer>().SetPosition(0, forest[edges[i].points[0].treeID].transform.position);
-                connection.GetComponent<LineRenderer>().SetPosition(1, forest[edges[i].points[1].treeID].transform.position);
+                GameObject connection = Instantiate(connectionPrefab, connectionRoot);
+                connection.GetComponent<LineRenderer>().positionCount = 2;
+                connection.GetComponent<LineRenderer>().SetPosition(0, drawForest[edges[i].points[0].treeID].transform.position);
+                connection.GetComponent<LineRenderer>().SetPosition(1, drawForest[edges[i].points[1].treeID].transform.position);
                 connectionRenderers.Add(connection);
-            }*/
+            }
         }
 
         private void SpawnTrees(List<Tree> _forest)
@@ -105,7 +96,7 @@ namespace ShortestOpenPath_Algorithm
                 _forest[i].treeID = i;
                 GameObject _tree = Instantiate(treePrefab, treeRoot);
                 _tree.transform.localPosition = new Vector3((float)_forest[i].coordinates[0] * 0.1f, (float)_forest[i].coordinates[1] * 0.1f);
-                forest_.Add(_tree);
+                drawForest.Add(_tree);
             }
         }
 
@@ -157,13 +148,11 @@ namespace ShortestOpenPath_Algorithm
 
         public void SetNearestNeighbour(Tree currentTree, List<Tree> forest)
         {
-            print("===========================");
             int nearestTreeId = -1;
             double minDistance = -1;
             List<double> distance = new List<double>();
             for (int i = 0; i < forest.Count; i++)
             {
-                print("i: " + i);
                 if (i == currentTree.treeID)
                 {
                     //distance.Add(0);
@@ -177,51 +166,28 @@ namespace ShortestOpenPath_Algorithm
                 distance.Add(Math.Sqrt(sum));
 
                 if (minDistance == -1)
-                    minDistance = sum;
-                bool isConnected = false;
-                foreach (var item in edges)
                 {
-                    Debug.Log(item + " | curr: " + currentTree.treeID + " | i: " + i);
-                    if (item.indexes.Contains(i) && item.indexes.Contains(currentTree.treeID))
-                    {
-                        //Debug.Log(item + " | curr: " + currentTree.treeID + " | i: " + i);
-                        print("Connected with: " + i);
-                        isConnected = true;
-                        break;
-                    }
-                    continue;
+                    minDistance = sum;
+                    nearestTreeId = i;
                 }
 
-                bool doSave = minDistance > sum && !isConnected;
-                print("i: " + i + " | minDist: " + minDistance + " | sum: " + sum);
+                bool doSave = minDistance > sum;
                 nearestTreeId = doSave ? i : nearestTreeId;
                 minDistance = doSave ? sum : minDistance;
             }
 
-            if (nearestTreeId == -1 || minDistance == -1)
+            foreach (var item in edges)
             {
-                print(currentTree.treeID);
-                print(nearestTreeId);
-                print(minDistance);
-                print("------------------");
-                return;
+                if (item.indexes.Contains(nearestTreeId) && item.indexes.Contains(currentTree.treeID))
+                    return;
+                continue;
             }
+
+            if (nearestTreeId == -1 || minDistance == -1)
+                return;
 
             Edge edge = new Edge(currentTree, forest[nearestTreeId]);
             edges.Add(edge);
-
-            // draw edge
-            if (edge.points[0].treeID == edge.points[1].treeID)
-                return;
-
-            GameObject connection = Instantiate(connectionPrefab, connectionRoot);
-            connection.GetComponent<LineRenderer>().positionCount = 2;
-            connection.GetComponent<LineRenderer>().SetPosition(0, forest_[edge.points[0].treeID].transform.position);
-            connection.GetComponent<LineRenderer>().SetPosition(1, forest_[edge.points[1].treeID].transform.position);
-            connectionRenderers.Add(connection);
-
-
-            SetNearestNeighbour(trees[nearestTreeId], trees);
         }
 
         /*
